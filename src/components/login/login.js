e44962024bcecfe054c5865173895fe1c5c6d6e1
mastergokul <selvarajgokul09@gmail.com>
2022-06-06 19:21:03 +0530

    fixes some errors

---
+++ src/components/login/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbars from "../navbar/navbar";
+import DemoNavbar from "../Navbars/DemoNavbar";
+
+ export default function Login(){
+    return (
+        
+    );
+};
